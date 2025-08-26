6c0c847168ac1babd4d67c846fde17046690967b
aVc <cresencioabcdef26@gmail.com>
2025-08-26 09:05:42 +0800

    addtional clean up

--- app/core-values/page.tsx
+++ app/core-values/page.tsx
@@ -1,12 +1,6 @@
 "use client";
 
 import Image from "next/image";
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import FadeInSection from "@/components/FadeInSection";
 
 export default function CoreValues() {
